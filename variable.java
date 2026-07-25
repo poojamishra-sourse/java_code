96fbe13e3a4ec97bed37d45620dc5dd1b3db1d5f
Pooja Mishra <pooja.mishra9456@gmail.com>
2026-07-25 17:09:54 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,19 @@
+public class variable {
+    public static void main(String[] args){
+    String name="john";
+    int marks=20;
+    double percent=55.22;
+    char grade='a';
+    boolean result=true;
+
+
+
+    System.out.println(" my name is "+name );
+    System.out.println("my marks is "+marks);
+    System.out.println("my percentage is "+percent);
+    System.out.println("my grade is "+grade);
+    System.out.println("my result is "+result);
+    System.out.println("hello java");
+
+    }
+}
